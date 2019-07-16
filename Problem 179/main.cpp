d6ef5273229f374f83a0a0aa20c128dcf514a8f8
punkproger <punkproger@gmail.com>
2019-07-16 22:09:17 +0300

    Stolen 2.32sec solution

--- Problem 179/main.cpp
+++ Problem 179/main.cpp
@@ -4,33 +4,21 @@
 #include <iostream>
 #include <cmath>
 
-using ull = unsigned int;
+using ull = int;
 
-inline size_t divisorsCount(ull val) {
-	size_t result{2};
-	for(int i = 2; i <= sqrt(val); ++i) {
-		if(val % i == 0) {
-			result += i*i != val? 2 : 1;
-		}
-	}
-
-	return result;
-}
 
 int main() {
 	constexpr ull limit = 1e7;
 	size_t result{0};
-	std::vector<size_t> divisors_counts(limit);
-	bool wasSameDivisorsCountBefore = false;
-	for(ull i = 2; i < limit; ++i) {
-		divisors_counts[i] = divisorsCount(i);
-
-		if(divisors_counts[i] == divisors_counts[i-1]) {
-			result += 1;
-		} 
-
-		if(i % 10000== 0) {
-			std::cout << i << std::endl;
+	std::vector<int> divnums(limit);
+	
+	for (int i = 1; i < 10000000; ++i) {
+		divnums[i] += 1;
+		for (int j = 2; (1+i)*j < 10000000; ++j) {
+			divnums[(i+1)*j] += 1;
+		}
+		if (i > 1) {
+			result += (divnums[i] == divnums[i-1]);
 		}
 	}
 
