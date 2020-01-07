41d83a2250cd1538ac102ff354d509566c7d38ee
punkproger <punkproger@gmail.com>
2020-01-07 13:08:51 +0200

    [174] Stolen idea 0,005s

--- Problem 174/main.cpp
+++ Problem 174/main.cpp
@@ -1,66 +1,41 @@
 #include <iostream>
-#include <cmath>
-#include <unordered_map>
+#include <array>
+#include <algorithm>
 
 using ull = int;
 
-inline ull getTylesCount(ull n, ull n_hole)
-{
-	return n*n - n_hole*n_hole;
-}
-
-inline ull getLType(ull tyles)
-{
-	constexpr ull c_limit = 1000000;
-	ull i, j, result{0};
-
-	const ull i_start = sqrt(tyles);
-
-	for (i = i_start;; i += 1)
-	{
-		const ull j_lim = (i%2 == 0 ? 2 : 1);
-		const ull j_start = (i%2 == 0 ? 2 : 1);
-
-
-		/*if (getTylesCount(i, j_lim) < tyles)
-		{
-			continue;
-		}*/
-		if (getTylesCount(i, i-2) > tyles)
-		{
-			break;
-		}
-
-		for (j = i-2; j >= j_lim; j -= 2)
-		{
-			ull t = getTylesCount(i, j);
-			
-			if (t == tyles)
-			{
-				++result;
-			}
-			else if (t > tyles)
-			{
-				break;
-			}
-		}
-	}
-
-	return result;
-}
 
 int main()
 {
+	constexpr ull c_limit{1000001};
+	constexpr ull c_square_limit{250002};
 
-	int result{0};
-	for (ull i = 8; i <= 1000000; i += 4)
+	std::array<ull, c_limit> N;
+	N.fill(0);
+	std::array<ull, c_square_limit> squares;
+
+	for (ull i = 1; i < squares.size(); ++i)
 	{
-		ull tyles = getLType(i);
-		if (tyles >= 1 && tyles <= 10)
+		squares[i] = i*i;
+	}
+
+	for (ull i = 3; i < c_square_limit; ++i)
+	{
+		for (ull j = i-2; j > 0 && squares[i] - squares[j] < c_limit; j -= 2)
+		{
+			++N[squares[i] - squares[j]];
+		}
+	}
+
+	ull result{0};
+	for (ull i = 0; i < N.size(); ++i)
+	{
+		if (N[i] >= 1 && N[i] <= 10)
 		{
 			++result;
 		}
 	}
+
 
 	std::cout << result << std::endl;
 
